c3fffe438292951899cccd7787deab5ca4f92338
shinkeonkim <singun11@kookmin.ac.kr>
2024-07-14 20:23:14 +0900

    feat: CORS 세팅

--- backend/network/network.go
+++ backend/network/network.go
@@ -3,6 +3,7 @@ package network
 import (
 	"log"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,16 @@ type Network struct {
 
 func NewServer() *Network {
 	n := &Network{engin: gin.New()}
+
+	n.engin.Use(gin.Logger())   // Log all requests to the console
+	n.engin.Use(gin.Recovery()) // Recover from any panics
+	n.engin.Use(cors.New(cors.Config{
+		AllowWebSockets:  true,
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT"},
+		AllowHeaders:     []string{"*"},
+		AllowCredentials: true,
+	})) // CORS Settings
 
 	return n
 }
